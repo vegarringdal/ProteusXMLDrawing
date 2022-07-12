cb607ae9717aeab4523f292691421d33439edaa1
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-07-12 22:14:24 +0200

    chore: add some code to pick up attributes we have not added

--- src/proteusXmlDrawing/utils/findMissing.ts
+++ src/proteusXmlDrawing/utils/findMissing.ts
@@ -1,24 +1,47 @@
 const mapElements = new Map<string, Set<string>>();
+const mapAttributes = new Map<string, Set<string>>();
 const elementNamesMap = new Map<string, string>();
+const attributeNamesMap = new Map<string, string>();
 
 export function collectMissingParts(element: Element, ctx: any) {
-    const keys = new Set(Object.keys(ctx).map((e) => e.toLowerCase()));
-    const elementNames = new Set<string>();
+    {
+        const keys = new Set(Object.keys(ctx).map((e) => e.toLowerCase()));
+        const elementNames = new Set<string>();
 
-    for (let i = 0; i < element.children.length; i++) {
-        elementNames.add(element.children[i].tagName.toLowerCase());
-        elementNamesMap.set(element.children[i].tagName.toLowerCase(), element.children[i].tagName);
+        for (let i = 0; i < element.children.length; i++) {
+            elementNames.add(element.children[i].tagName.toLowerCase());
+            elementNamesMap.set(
+                element.children[i].tagName.toLowerCase(),
+                element.children[i].tagName
+            );
+        }
+
+        const elementSet = mapElements.get(element.tagName) || new Set();
+
+        elementNames.forEach((tagname) => {
+            if (!keys.has(tagname)) {
+                elementSet.add(tagname);
+            }
+        });
+
+        mapElements.set(element.tagName, elementSet);
     }
 
-    const elementSet = mapElements.get(element.tagName) || new Set();
+    {
+        const keys = new Set(Object.keys(ctx).map((e) => e.toLowerCase()));
+        const attributes = element.getAttributeNames();
 
-    elementNames.forEach((tagname) => {
-        if (!keys.has(tagname)) {
-            elementSet.add(tagname);
-        }
-    });
+        const attributeSet = mapAttributes.get(element.tagName) || new Set();
 
-    mapElements.set(element.tagName, elementSet);
+        attributes.forEach((name) => {
+            if (!keys.has(name.toLowerCase())) {
+                attributeSet.add(name.toLowerCase());
+                attributeNamesMap.set(name.toLowerCase(), name);
+            }
+        });
+
+        mapAttributes.set(element.tagName, attributeSet);
+    }
 }
 
 export function printMissing() {
@@ -26,9 +49,22 @@ export function printMissing() {
         const missingTags = mapElements.get(tagName);
         if (missingTags?.size) {
             console.log("-------------------------------");
+            console.log("--ELEMENT");
             console.log("----", tagName);
             missingTags.forEach((key) => {
                 console.log("--------->", elementNamesMap.get(key));
+            });
+        }
+    });
+
+    Array.from(mapAttributes.keys()).forEach((attrName) => {
+        const missingTags = mapAttributes.get(attrName);
+        if (missingTags?.size) {
+            console.log("-------------------------------");
+            console.log("--ATTRIBUTE");
+            console.log("----", attrName);
+            missingTags.forEach((key) => {
+                console.log("--------->", attributeNamesMap.get(key));
             });
         }
     });
