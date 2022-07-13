8b78415f3a7c72e9c69ef31c66394cfe8fc4c402
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-07-13 23:45:30 +0200

    chore: add missing draw

--- src/proteusXmlDrawing/children/MetaData.ts
+++ src/proteusXmlDrawing/children/MetaData.ts
@@ -1,9 +1,9 @@
-
 import { collectMissingParts } from "../utils/findMissing";
 import { getElements } from "../utils/getElement";
 import { GenericAttributes } from "./GenericAttributes";
 import { Label } from "./Label";
 import { Component } from "./Component";
+import { getDrawable } from "../utils/callDrawOnChildren";
 
 export class MetaData {
     isChild = true;
@@ -18,5 +18,12 @@ export class MetaData {
         this.label = getElements(element, "Label", Label);
         this.component = getElements(element, "Component", Component);
         collectMissingParts(this.element, this);
+    }
+
+    public draw(unit: number, pageOriginX: number, pageOriginY: number, offsetX = 0, offsetY = 0) {
+        const drawables = getDrawable(this);
+        drawables.forEach((drawable) => {
+            drawable.draw(unit, pageOriginX, pageOriginY, offsetX, offsetY);
+        });
     }
 }
