2bba1d41c98f99ae5c8bf2bcd94316830a85aeb0
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-08-09 14:58:33 +0200

    chore: add trimmed ellipse

--- src/proteusXmlDrawing/drawTrimmedCurve.ts
+++ src/proteusXmlDrawing/drawTrimmedCurve.ts
@@ -101,43 +101,68 @@ export function drawTrimmedCurve(
             }
         }
         if (drawable.element.tagName === "Ellipse") {
-            //I dont know how to create this yet
-            console.warn("Trimmed curve ellipse not implemented", ctx);
+            //used circle and scaled it... looks like its working..
 
             const Point = getPaper().Point;
             const Path = getPaper().Path;
             const Color = getPaper().Color;
-            const Size = getPaper().Size;
-            const Rectangle = getPaper().Rectangle;
+
             const comp = drawable as unknown as Ellipse;
 
             const x = comp.Position[0].Location[0].x.valueAsNumber + offsetX;
             const y = comp.Position[0].Location[0].y.valueAsNumber + offsetY;
             const point = new Point(x * unit, pageOriginY * unit - y * unit);
+            const radius = comp.primaryAxis.valueAsNumber * unit;
 
-            const rectangle = new Rectangle(
-                point,
-                new Size(
-                    comp.primaryAxis.valueAsNumber * unit,
-                    comp.secondaryAxis.valueAsNumber * unit
-                )
-            );
-            const ellipse = new Path.Ellipse(rectangle);
+            const startAngle = ctx.startAngle?.valueAsNumber || 0;
+            const endAngle = ctx.endAngle?.valueAsNumber || 0;
 
-            ellipse.strokeColor = new Color({
+            // create vec2 with angle 0 and update it
+            const from = new Point(radius, 0);
+            from.angle = startAngle;
+
+            // create vec2 with angle 0 and update it
+            const to = new Point(radius, 0);
+            to.angle = endAngle;
+
+            // create vec2 with angle 0 and update it with position between start and end
+            const through = new Point(radius, 0);
+            const diff = endAngle - startAngle;
+            through.angle = startAngle + diff / 2;
+
+            const arc = new Path.Arc(from, through, to);
+            arc.strokeColor = new Color({
                 red: comp.Presentation[0].r.value,
                 green: comp.Presentation[0].g.value,
                 blue: comp.Presentation[0].b.value
             });
 
-            ellipse.strokeWidth = comp.Presentation[0].lineWeight.valueAsNumber * unit;
+            arc.strokeWidth = comp.Presentation[0].lineWeight.valueAsNumber * unit;
+
+            arc.translate(point);
+            arc.scale(1, (comp.secondaryAxis.valueAsNumber * unit) / radius, point);
 
             if (group) {
-                group.addChild(ellipse);
+                group.addChild(arc);
+            } else {
+                arc.onClick = function () {
+                    console.log(ctx);
+                };
             }
 
-            if (comp.filled?.value) {
-                ellipse.fillColor = new Color("black");
+            if (debug.ellipse) {
+                const Size = getPaper().Size;
+                const Shape = getPaper().Shape;
+                const size = new Size(arc.bounds.width, arc.bounds.height);
+                const shape = new Shape.Rectangle(new Point(arc.bounds.x, arc.bounds.y), size);
+                shape.fillColor = new Color({ red: 1, green: 0, blue: 1, alpha: 0.5 });
+                shape.onClick = () => {
+                    console.log(ctx);
+                };
+                shape.bringToFront();
+                if (group) {
+                    group.addChild(shape);
+                }
             }
         }
     });
