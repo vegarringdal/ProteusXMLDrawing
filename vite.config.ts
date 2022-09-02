5e20d3cb5f5592c607b75d246bb48415639a4f52
Vegar Ringdal <vegar.ringdal@aibel.com>
2022-09-02 09:28:02 +0200

    chore: fix demo pages "docs" folder

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig(({ command }) => {
                 assetsDir: "./",
                 target: "es2015",
                 emptyOutDir: true,
-                outDir: "./docs"
+                outDir: "../docs"
             }
         };
     }
