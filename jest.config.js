e77d7ff88c84898a87de5383704219b2b982415b
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-09-25 20:40:16 +0200

    chore: clean up dependencies/jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,14 @@
 module.exports = {
-    /*    preset: "ts-jest", */
     testEnvironment: "jsdom",
     clearMocks: true,
-    maxConcurrency: 1,
-    /* coverageProvider: "v8", */
-    setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.js"],
     collectCoverageFrom: [],
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
-        "^.+\\.svg$": "<rootDir>/jest/svgTransform.js",
-        "^.+\\.css$": "<rootDir>/jest/cssTransform.js"
-    },
-    globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/jest/tsconfig.jest.json",
-            diagnostics: false
-        }
+        "^.+\\.(js|jsx|ts|tsx)$": [
+            "ts-jest",
+            {
+                tsconfig: "<rootDir>/tsconfig.jest.json",
+                diagnostics: false
+            }
+        ]
     }
 };
--- jest/jest.setup.js
+++
@@ -1,5 +0,0 @@
-// The @testing-library/jest-dom library provides a set of
-// custom jest matchers that you can use to extend jest.
-// These will make your tests more declarative, clear to read and to maintain.
-// check out: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
